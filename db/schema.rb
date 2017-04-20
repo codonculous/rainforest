ef406f085dc08cd4d537e2b68609373753301ebb
codonculous <feidameng01@gmail.com>
2017-04-20 01:08:24 -0400

    create user model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420030453) do
+ActiveRecord::Schema.define(version: 20170420050640) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170420030453) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "product_img_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
